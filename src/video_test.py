50deb1d5ddb8545a30ff136d33f23ad54908595e
zackkracky <48663430+zackkracky@users.noreply.github.com>
2025-12-17 02:01:07 +0530

    video and frame test edit

---
+++ src/video_test.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture(0,cv2.CAP_V4L2)
+
+if not cap.isOpened():
+    print("error in launching cam")
+    rasie SystemExit
+else:
+    prints("sucess")
+    
+    while True:
+        status , frame = cap.read()
+
+        if not status:
+            continue
+
+        cv2.imshow("live",frame)
+
+        if waitKey(1) & 0xFF == ord('q'):
+            break
+cv2.release()
+cv2.destroyAllWindows()
+
