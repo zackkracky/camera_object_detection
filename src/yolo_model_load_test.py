d75e81b2a463946726258bfabeae9b9b4ccbf2d8
zackkracky <battuarnav030@gmail.com>
2025-12-26 14:51:24 +0530

    error fix

--- src/yolo_model_load_test.py
+++ src/yolo_model_load_test.py
@@ -2,8 +2,8 @@ import cv2
 
 #here we are using ../ in the start so it goes back to the predecessor folder then comes to model folder
 
-cfg_path = "../models/yolo4-tiny.cfg"
-weight_path = "../models/yolo4-tiny.weights"
+cfg_path = "../models/yolov4-tiny.cfg"
+weight_path = "../models/yolov4-tiny.weights"
 
 net = cv2.dnn.readNetFromDarknet(cfg_path,weight_path)
 
