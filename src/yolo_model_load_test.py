52b34153c679a0f2a7c9227f01d71ca5352864c9
zackkracky <battuarnav030@gmail.com>
2025-12-26 14:47:03 +0530

    directing fix

--- src/yolo_model_load_test.py
+++ src/yolo_model_load_test.py
@@ -1,7 +1,9 @@
 import cv2
 
-cfg_path = "models/yolo4-tiny.cfg"
-weight_path = "models/yolo4-tiny.weights"
+#here we are using .. in the start so it goes back to the predecessor folder then comes to model folder
+
+cfg_path = "..models/yolo4-tiny.cfg"
+weight_path = "..models/yolo4-tiny.weights"
 
 net = cv2.dnn.readNetFromDarknet(cfg_path,weight_path)
 
